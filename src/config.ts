352d7783638a99c1f334cd2ce2d23e627eec8996
REAZN <reasonwhygamer@gmail.com>
2022-11-25 09:31:07 +0000

    configs

---
+++ src/config.ts
@@ -0,0 +1,20 @@
+
+export const links = ["home", "about", "services", "find us"];
+
+export const social = [
+    {
+        title: "TikTok",
+        url: "https://tiktok.com/",
+        icon: "ic:baseline-tiktok",
+    },
+    {
+        title: "Facebook",
+        url: "https://facebook.com/",
+        icon: "ri:facebook-fill",
+    },
+    {
+        title: "Instagram",
+        url: "https://instagram.com/",
+        icon: "mdi:instagram",
+    }
+]
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,19 @@
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			fontFamily: {
+				"inter": ["Inter", "sans-serif"],
+			},
+			colors: {
+				text: "#222222",
+				background: "#FAFAF6",
+				backgroundAlt: "#EDE8E2",
+				accent: "#E3B8B5",
+				accent2: "#DA948F",
+			}
+		},
+
 	},
 	plugins: [],
 }
