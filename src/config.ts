67514ff65080c100313fc194701f0690e60f9033
REAZN <reasonwhygamer@gmail.com>
2022-11-29 12:55:05 +0000

    misc

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,5 @@
 
-export const links = ["home", "about", "services", "find us"];
+export const links: string[] = ["home", "about", "feed", "services", "find us"];
 
 export const details = {
     address: "390 Vicarage Lane\nBlackpool\nFY4 4LP",
@@ -28,9 +28,9 @@ export const social = [
         url: "https://www.instagram.com/emzsalon",
         icon: "mdi:instagram",
     }
-]
+] satisfies {[key: string]: string}[];
 
-// TODO - compress images
+// TODO - compress images (webp)
 export const posts = [
     {
         url: "https://www.instagram.com/p/CjTb1jCrmKH/",
@@ -52,4 +52,24 @@ export const posts = [
         url: "https://www.facebook.com/EmzSalon/photos/a.720990101351079/3883419685108089/",
         image: "/images/posts/5.jpg",
     },
-]
+    {
+        url: "https://www.instagram.com/p/CjTb1jCrmKH/",
+        image: "/images/posts/1.jpg",
+    },
+    {
+        url: "https://www.facebook.com/photo.php?fbid=539414658185028",
+        image: "/images/posts/2.jpg",
+    },
+    {
+        url: "https://www.facebook.com/photo.php?fbid=559212249538602",
+        image: "/images/posts/3.jpg",
+    },
+    {
+        url: "https://www.facebook.com/photo/?fbid=380022964124199",
+        image: "/images/posts/4.jpg",
+    },
+    {
+        url: "https://www.facebook.com/EmzSalon/photos/a.720990101351079/3883419685108089/",
+        image: "/images/posts/5.jpg",
+    },
+] satisfies {[key: string]: string}[];
