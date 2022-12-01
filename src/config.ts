45686b53d3ba4cb6ad6c733d2021489750950b61
REAZN <reasonwhygamer@gmail.com>
2022-12-01 19:55:18 +0000

    feed images

--- src/config.ts
+++ src/config.ts
@@ -15,7 +15,7 @@ export const social = [
     },
     {
         title: "TikTok",
-        url: "https://tiktok.com/",
+        url: "https://www.tiktok.com/@emilyvictoria93",
         icon: "ic:baseline-tiktok",
     },
     {
@@ -49,27 +49,27 @@ export const posts = [
         image: "/images/posts/4.webp",
     },
     {
-        url: "https://www.facebook.com/EmzSalon/photos/a.720990101351079/3883419685108089/",
+        url: "https://www.facebook.com/photo/?fbid=578746957585131",
         image: "/images/posts/5.webp",
     },
     {
-        url: "https://www.instagram.com/p/CjTb1jCrmKH/",
-        image: "/images/posts/1.webp",
+        url: "https://www.facebook.com/photo/?fbid=584319193694574",
+        image: "/images/posts/6.webp",
     },
     {
-        url: "https://www.facebook.com/photo.php?fbid=539414658185028",
-        image: "/images/posts/2.webp",
+        url: "https://www.facebook.com/photo/?fbid=578746900918470",
+        image: "/images/posts/7.webp",
     },
     {
-        url: "https://www.facebook.com/photo.php?fbid=559212249538602",
-        image: "/images/posts/3.webp",
+        url: "https://www.facebook.com/photo/?fbid=4366073693509350",
+        image: "/images/posts/8.webp",
     },
     {
-        url: "https://www.facebook.com/photo/?fbid=380022964124199",
-        image: "/images/posts/4.webp",
+        url: "https://www.facebook.com/photo/?fbid=4242130305903690",
+        image: "/images/posts/9.webp",
     },
     {
-        url: "https://www.facebook.com/EmzSalon/photos/a.720990101351079/3883419685108089/",
-        image: "/images/posts/5.webp",
+        url: "https://www.facebook.com/photo/?fbid=4842524902530891",
+        image: "/images/posts/10.webp",
     },
 ] satisfies {[key: string]: string}[];
